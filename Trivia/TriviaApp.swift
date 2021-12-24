bd8039229c94aca8cc28af500a10d6658cacbaf2
temilaj <temilaj@hotmail.com>
2021-12-24 17:27:59 -0500

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Temi Lajumoke on 12/24/21.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
